9f8f6d1bd265e7524628cd9a2a5be21ed91d867e
3illBaby <thrillanon@outlook.com>
2025-05-17 22:11:20 +0100

    chore(config.ts): added config

---
+++ src/shared/config/config.ts
@@ -0,0 +1,8 @@
+export default () => ({
+  redis: {
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
