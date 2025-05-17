f70cf87d92f2732fba6cc923b9bc60de299fc2eb
3illBaby <thrillanon@outlook.com>
2025-05-17 23:01:21 +0100

    feat(automation.provider.ts): impl func to handle queue

--- src/modules/automation/provider/automation.provider.ts
+++ src/modules/automation/provider/automation.provider.ts
@@ -1,4 +1,26 @@
+import { StellarService } from '@/modules/stellar/service/stellar.service';
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
+import { IHandleQueue } from '../interface/automation.interface';
 
 @Injectable()
-export class AutomationProvider {}
+export class AutomationProvider {
+  constructor(
+    @InjectQueue('transfer') private transferQueue: Queue,
+    private readonly stellarService: StellarService,
+  ) {}
+
+  async addToQueue(args: IHandleQueue) {
+    return this.transferQueue.add('init-transfer', args.data, {
+      delay: args.delay,
+      attempts: 3,
+      backoff: 500,
+      removeOnComplete: true,
+    });
+  }
+
+  async executeTransfer(args: Omit<IHandleQueue, 'delay'>) {
+    return await this.stellarService.transfer(args.data);
+  }
+}
