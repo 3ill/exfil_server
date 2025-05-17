4d0618087a5bee8afba6d5c561c949fe53db673d
3illBaby <thrillanon@outlook.com>
2025-05-17 22:12:00 +0100

    chore(app.module.ts): registered bull

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { BullModule } from '@nestjs/bull';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './shared/config/config';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), EventEmitterModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [config],
+    }),
+    ScheduleModule.forRoot(),
+    EventEmitterModule.forRoot(),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('redis.host'),
+          port: configService.get('redis.port'),
+          username: configService.get('redis.username'),
+          password: configService.get('redis.password'),
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
