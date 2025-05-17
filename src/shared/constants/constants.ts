34069d4f4f68e4a4bf830c96f123a4d9f5228170
3illBaby <thrillanon@outlook.com>
2025-05-17 20:20:04 +0100

    chore(constants.ts): added constant

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,2 +1,6 @@
 export const TESTNET_URL = 'https://api.testnet.minepi.com';
 export const MAINNET_URL = 'https://api.mainnet.minepi.com';
+export const PUBLIC_KEY =
+  'GDPG2HQKWMSKOM7SOAEXNOUVZOKBOOQX2ALDFXRXPHUUHUR3SEQN2PEY';
+export const SECRET_KEY =
+  'SCF4S3GJ623TYPWRHCFWD3ARXEDMPGDB3BRGRT3CIK4GFRZS6YWDVEN3';
