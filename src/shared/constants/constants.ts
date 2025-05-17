d49dda92023077f86d6f1e25192d65796458f5d6
3illBaby <thrillanon@outlook.com>
2025-05-17 21:41:13 +0100

    chore(package.json): added packages

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -4,3 +4,6 @@ export const PUBLIC_KEY =
   'GDPG2HQKWMSKOM7SOAEXNOUVZOKBOOQX2ALDFXRXPHUUHUR3SEQN2PEY';
 export const SECRET_KEY =
   'SCF4S3GJ623TYPWRHCFWD3ARXEDMPGDB3BRGRT3CIK4GFRZS6YWDVEN3';
+export const DERIVATION_PATH = "m/44'/314159'/0'";
+export const PASSPHRASE =
+  'kitchen palace loop code seed era nominee solve online trust humor jar economy fade relax truth kingdom extend detect surprise badge muffin person weird';
