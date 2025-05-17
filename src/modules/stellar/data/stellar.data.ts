5b9221bc390d5ee572c004e23a84d38da759fc22
3illBaby <thrillanon@outlook.com>
2025-05-17 20:16:32 +0100

    chore(stellar.data.ts): added constant data

--- src/modules/stellar/data/stellar.data.ts
+++ src/modules/stellar/data/stellar.data.ts
@@ -1,5 +1,15 @@
-export enum StellarSuccessMessages {}
+export enum PiNetworkPassphrase {
+  MAINNET = 'Pi Mainnet',
+  TESTNET = 'Pi Testnet',
+}
+
+export enum StellarSuccessMessages {
+  SUCCESS_CREATING_ACCOUNT = 'Account created successfully',
+}
 
 export enum StellarErrorMessages {
   ERROR_SUBMITTING_TX = 'Error submitting transaction',
+  ERROR_CREATING_KEYPAIR = 'Error creating keypair',
+  ERROR_CREATING_ACCOUNT = 'Error creating account',
+  ERROR_INVALID_NETWORK = 'Invalid network',
 }
