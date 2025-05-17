c12e2f5a573ff35cc7182d194ecae6f73e46aa8d
3illBaby <thrillanon@outlook.com>
2025-05-17 23:22:08 +0100

    chore(automation.service.ts): consumed provider

--- src/modules/automation/service/automation.service.ts
+++ src/modules/automation/service/automation.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { AutomationProvider } from '../provider/automation.provider';
+import { IScheduleTransfer } from '../interface/automation.interface';
 
 @Injectable()
-export class AutomationService {}
+export class AutomationService {
+  constructor(private readonly automationProvider: AutomationProvider) {}
+
+  async executeTransfer(args: IScheduleTransfer) {
+    try {
+      const result = await this.automationProvider.scheduleTransfer(args);
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'An error occurred while executing transfer',
+        HttpStatus.OK,
+        {
+          cause: error,
+        },
+      );
+    }
+  }
+}
