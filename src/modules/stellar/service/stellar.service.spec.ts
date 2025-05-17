22dc85e0f37283f138e3c590eb78408900cec35f
3illBaby <thrillanon@outlook.com>
2025-05-17 21:42:48 +0100

    test(stellar.service.spec.ts): added transfer test case

--- src/modules/stellar/service/stellar.service.spec.ts
+++ src/modules/stellar/service/stellar.service.spec.ts
@@ -1,14 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StellarService } from './stellar.service';
 import { StellarProvider } from '../provider/stellar.provider';
-import { PUBLIC_KEY } from '@/shared/constants/constants';
+import { PASSPHRASE, PUBLIC_KEY } from '@/shared/constants/constants';
+import { DeriveSecret } from '@/shared/utils/derive-secret.utils';
 
 describe('StellarService', () => {
   let service: StellarService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StellarService, StellarProvider],
+      providers: [StellarService, StellarProvider, DeriveSecret],
     }).compile();
 
     service = module.get<StellarService>(StellarService);
@@ -19,7 +20,7 @@ describe('StellarService', () => {
   });
 
   describe('balances', () => {
-    it('should return balances', async () => {
+    it.only('should return balances', async () => {
       const balances = await service.checkBalance({
         network: 'TESTNET',
         publicKey: PUBLIC_KEY,
@@ -31,7 +32,7 @@ describe('StellarService', () => {
   });
 
   describe('Create Account', () => {
-    it.only('should successfully create a pi account and fund it', async () => {
+    it('should successfully create a pi account and fund it', async () => {
       const result = await service.createAccount({
         network: 'TESTNET',
         sourceAddress: PUBLIC_KEY,
@@ -40,6 +41,20 @@ describe('StellarService', () => {
 
       console.log(result.data);
       expect(typeof result.data).toBe('object');
+    }, 50000);
+  });
+
+  describe('Transfer', () => {
+    it('should successfully transfer Pi from a given account using passphrase', async () => {
+      const result = await service.transfer({
+        passphrase: PASSPHRASE,
+        destinationAddress: PUBLIC_KEY,
+        amount: '5',
+        network: 'TESTNET',
+      });
+
+      console.log(result);
+      expect(result).toBeDefined();
     }, 50000);
   });
 });
