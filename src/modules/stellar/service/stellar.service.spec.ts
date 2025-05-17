b37cc30efb5bb108565ba7bfa475cfd2314a781c
3illBaby <thrillanon@outlook.com>
2025-05-17 20:18:38 +0100

    test(stellar.service.spec.ts): added test case to create account

--- src/modules/stellar/service/stellar.service.spec.ts
+++ src/modules/stellar/service/stellar.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StellarService } from './stellar.service';
+import { StellarProvider } from '../provider/stellar.provider';
+import { PUBLIC_KEY } from '@/shared/constants/constants';
 
 describe('StellarService', () => {
   let service: StellarService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StellarService],
+      providers: [StellarService, StellarProvider],
     }).compile();
 
     service = module.get<StellarService>(StellarService);
@@ -14,5 +16,30 @@ describe('StellarService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('balances', () => {
+    it('should return balances', async () => {
+      const balances = await service.checkBalance({
+        network: 'TESTNET',
+        publicKey: PUBLIC_KEY,
+      });
+
+      console.log(balances);
+      expect(balances).toBeDefined();
+    });
+  });
+
+  describe('Create Account', () => {
+    it.only('should successfully create a pi account and fund it', async () => {
+      const result = await service.createAccount({
+        network: 'TESTNET',
+        sourceAddress: PUBLIC_KEY,
+        startingBalance: '10',
+      });
+
+      console.log(result.data);
+      expect(typeof result.data).toBe('object');
+    }, 50000);
   });
 });
