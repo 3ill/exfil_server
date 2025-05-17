f6d96e731507a28d4cb604b946f197e974ba4ebd
3illBaby <thrillanon@outlook.com>
2025-05-17 21:04:30 +0100

    test(stellar.provider.spec.ts): test case to derive keypair from passphrase

--- src/modules/stellar/provider/stellar.provider.spec.ts
+++ src/modules/stellar/provider/stellar.provider.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StellarProvider } from './stellar.provider';
+import { DeriveSecret } from '@/shared/utils/derive-secret.utils';
+import { PASSPHRASE } from '@/shared/constants/constants';
 
 describe('Stellar', () => {
   let provider: StellarProvider;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StellarProvider],
+      providers: [StellarProvider, DeriveSecret],
     }).compile();
 
     provider = module.get<StellarProvider>(StellarProvider);
@@ -17,8 +19,14 @@ describe('Stellar', () => {
   });
 
   describe('Keypair', () => {
-    it.only('should create keypair capable of sigining transactions', () => {
+    it('should create keypair capable of sigining transactions', () => {
       const result = provider.createKeypair();
+      console.log(result);
+      expect(result).toBeDefined();
+    });
+
+    it.only('should derive keypair from passphrase', async () => {
+      const result = await provider.deriveKeypairFromPassphrase(PASSPHRASE);
       console.log(result);
       expect(result).toBeDefined();
     });
