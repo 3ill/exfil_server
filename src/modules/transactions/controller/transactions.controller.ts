93452d4cb509ba25d7cc86a12a3114a1a05ac4ca
3illBaby <thrillanon@outlook.com>
2025-05-18 23:04:29 +0100

    feat(transactions.controller.ts): consumed service

--- src/modules/transactions/controller/transactions.controller.ts
+++ src/modules/transactions/controller/transactions.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TransactionsService } from '../service/transactions.service';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+  constructor(private readonly txService: TransactionsService) {}
+
+  @Get('transactions')
+  async fetchTxs() {
+    return await this.txService.fetchTxs();
+  }
+}
