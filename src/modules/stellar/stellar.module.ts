741ce8012af460f9d1cd7b54bdd8c29d4eb3131d
3illBaby <thrillanon@outlook.com>
2025-05-18 15:44:34 +0100

    chore(stellar.module.ts): exported provider and service

--- src/modules/stellar/stellar.module.ts
+++ src/modules/stellar/stellar.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StellarProvider } from './provider/stellar.provider';
 import { StellarService } from './service/stellar.service';
+import { DeriveSecret } from '@/shared/utils/derive-secret.utils';
 
 @Module({
-  providers: [StellarService, StellarProvider],
+  providers: [StellarService, StellarProvider, DeriveSecret],
+  exports: [StellarService, StellarProvider],
 })
 export class StellarModule {}
