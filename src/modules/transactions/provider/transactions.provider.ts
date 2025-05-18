7d20898b22f6ae3785b5182ca1f979a7a56622ee
3illBaby <thrillanon@outlook.com>
2025-05-18 22:59:11 +0100

    feat(transactions.provider.ts): impl func to create and fetch txs

--- src/modules/transactions/provider/transactions.provider.ts
+++ src/modules/transactions/provider/transactions.provider.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Transactions } from '../schema/transactions.schema';
 import { Model } from 'mongoose';
+import { CreateTxDto } from '@/shared/dto/events.dto';
+import { TxErrorMessage, TxSuccessMessage } from '../data/transactions.data';
 
 @Injectable()
 export class TransactionsProvider {
@@ -10,5 +12,46 @@ export class TransactionsProvider {
     private readonly transactionsModel: Model<Transactions>,
   ) {}
 
-  async createTransaction() {}
+  async createTxs(args: CreateTxDto) {
+    const { amount, destinationAddress, status, timestamp, hash } = args;
+    try {
+      const result = await this.transactionsModel.create({
+        amount,
+        destinationAddress,
+        status,
+        hash,
+        timestamp,
+      });
+
+      return {
+        status: HttpStatus.OK,
+        message: TxSuccessMessage.TX_CREATED,
+        data: result._id,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        TxErrorMessage.ERROR_CREATING_TX,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: error.message,
+        },
+      );
+    }
+  }
+
+  async fetchTxs() {
+    try {
+      return await this.transactionsModel.find();
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        TxErrorMessage.ERROR_FETCHING_TXS,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: error.message,
+        },
+      );
+    }
+  }
 }
