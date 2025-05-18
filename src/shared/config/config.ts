c8fee7751756256d32072d43276c26fb54cb2aa2
3illBaby <thrillanon@outlook.com>
2025-05-18 21:44:46 +0100

    chore(config.ts): added db config

--- src/shared/config/config.ts
+++ src/shared/config/config.ts
@@ -1,4 +1,8 @@
 export default () => ({
+  db: {
+    uri: process.env.MONGODB_URI,
+    name: process.env.DB_NAME,
+  },
   redis: {
     username: process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
