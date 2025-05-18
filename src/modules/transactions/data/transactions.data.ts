54106bac28377092a87bc3edf92afe64ce592ccc
3illBaby <thrillanon@outlook.com>
2025-05-18 22:58:50 +0100

    chore(transactions.data.ts): added tx data

--- src/modules/transactions/data/transactions.data.ts
+++ src/modules/transactions/data/transactions.data.ts
@@ -1,7 +1,9 @@
 export enum TxSuccessMessage {
   TX_CREATED = 'transaction created successfully',
+  TX_FETCHED = 'transactions fetched successfully',
 }
 
 export enum TxErrorMessage {
   ERROR_CREATING_TX = 'error creating transaction',
+  ERROR_FETCHING_TXS = 'error fetching transactions',
 }
