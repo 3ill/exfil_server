98d52fff3d33b57d578e18dbe502c6d1cb1a93e3
3illBaby <thrillanon@outlook.com>
2025-05-18 23:02:37 +0100

    feat(transactions.service.ts): listen to event and consume txProvider

--- src/modules/transactions/service/transactions.service.ts
+++ src/modules/transactions/service/transactions.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { TransactionsProvider } from '../provider/transactions.provider';
+
+import { CreateTxDto } from '@/shared/dto/events.dto';
+import { OnEvent } from '@nestjs/event-emitter';
+import { SharedEvents } from '@/shared/events/events';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+  constructor(private readonly txProvider: TransactionsProvider) {}
+
+  @OnEvent(SharedEvents.CREATE_TX)
+  async createTx(args: CreateTxDto) {
+    return await this.txProvider.createTxs(args);
+  }
+
+  async fetchTxs() {
+    return await this.txProvider.fetchTxs();
+  }
+}
